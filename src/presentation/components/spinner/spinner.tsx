72233d6578ec5c767867068110a1a9fa7f58b5ad
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-11-08 12:56:27 -0300

    feat: ensure login presents spinner on submit

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -5,7 +5,7 @@ type Props = React.HTMLAttributes<HTMLElement>
 
 const Spinner: React.FC = (props: Props) => {
   return (
-    <div className={styles.spinner} {...props}>
+    <div className={styles.spinner} {...props} data-testid="spinner">
         <div/><div/><div/><div/>
     </div>
   )
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -26,12 +26,17 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     })
   }, [state.email, state.password])
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    setState({ ...state, isLoading: true })
+  }
+
   return (
     <div className={styles.login}>
        <HeaderLogin />
 
       <FormContext.Provider value={{ state, setState }}>
-       <form className={styles.form} action="">
+       <form className={styles.form} action="" onSubmit={handleSubmit}>
         <h2>Login</h2>
 
         <Input type="email" name='email' placeholder='Digite seu email'/>
