4377503739a600e497c1f95842d00dea8dbfb194
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-11-08 12:21:46 -0300

    feat: ensure login show valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -21,11 +21,11 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Ok'
   }
 
   return (
