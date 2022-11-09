5e13df4bfb135a1133e4a06a65600a1ca8169ad3
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-11-09 11:57:43 -0300

    test: ensure email validation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -7,8 +7,15 @@ const makeSut = (): EmailValidation => new EmailValidation(faker.internet.email(
 describe('Email validation', () => {
   test('Should return error if email is invalid', () => {
     const sut = makeSut()
-    const error = sut.validate('')
+    const error = sut.validate(faker.random.word())
 
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.internet.email())
+
+    expect(error).toBeFalsy()
   })
 })
