c9c4b233d99c9d125afa34c4bfc404b8f4d61f2a
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-11-09 11:24:35 -0300

    test: ensure required field validation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { RequiredFieldError } from '@/validation/errors'
+import { faker } from '@faker-js/faker'
 
 describe('Required field validation', () => {
   test('Should return error if field is empty', () => {
@@ -7,5 +8,12 @@ describe('Required field validation', () => {
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+
+    expect(error).toBeFalsy()
   })
 })
