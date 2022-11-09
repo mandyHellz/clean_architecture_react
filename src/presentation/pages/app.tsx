7614bc39ca657b00412d7543101906139bb4e1bd
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-11-09 15:20:46 -0300

    feat: add login composition

--- src/presentation/pages/app.tsx
+++ src/presentation/pages/app.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Login } from '@/presentation/pages'
-import '@/presentation/styles/global.scss'
 
-const App: React.FC = () => {
+type Props = {
+  MakeLogin: React.FC
+}
+
+const App: React.FC<Props> = ({ MakeLogin }: Props) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login />}/>
+        <Route path='/login' element={<MakeLogin/>}/>
       </Routes>
     </BrowserRouter>
   )
