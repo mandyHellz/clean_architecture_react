76c30b97f6f812432d626b65ff434fdc44a299e5
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-11-24 11:46:48 -0300

    fix: removed duplicated test

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -14,14 +14,4 @@ describe('LocalStorageAdapter', () => {
 
     expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
   })
-
-  test('Should call localStorage with correct values', async () => {
-    const sut = new LocalStorageAdapter()
-    const key = faker.database.column()
-    const value = faker.random.word()
-
-    await sut.set(key, value)
-
-    expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
-  })
 })
