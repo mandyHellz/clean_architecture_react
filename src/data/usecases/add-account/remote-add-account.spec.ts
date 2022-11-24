6ca2fd28dfcbaa44ba14fb06bdf5fc3b95ee0801
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-11-24 14:28:16 -0300

    test: ensure RemoteAddAccount throws unexpected error if httpPostClient returns 500

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -68,4 +68,14 @@ describe('RemoteAddAccount', () => {
 
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Should throw unexpected error if httpPostClient returns 500', async () => {
+    const { httpPostClientSpy, sut } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.add(mockAddAccountParams())
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
